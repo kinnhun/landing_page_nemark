035f973c262c14b2393ab21a0ecb70b89d7ee60e
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-12-03 16:21:44 +0700

    test

--- landing-page-nemark/src/pages/_document.tsx
+++ landing-page-nemark/src/pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default function Document() {
+const MyDocument = () => {
   return (
     <Html lang="en">
       <Head>
@@ -23,5 +25,33 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
+
+export default MyDocument;
